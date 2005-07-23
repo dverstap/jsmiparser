142f977a9985936f2400d7d4e3462694f18a0f71
Davy Verstappen <davy@davyverstappen.me>
2005-07-23 15:26:26 +0000

    git-svn-id: http://jsmiparser.svn.sf.net/svnroot/jsmiparser/trunk@29 5cfb4343-f3fb-0310-ad28-926a00f43e72

--- jsmiparser-api/src/test/java/org/jsmiparser/smi/UtilTest.java
+++ jsmiparser-api/src/test/java/org/jsmiparser/smi/UtilTest.java
@@ -13,10 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jsmiparser.smi.model;
+package org.jsmiparser.smi;
 
 import junit.framework.TestCase;
-import org.jsmiparser.smi.SmiUtil;
 
 public class UtilTest extends TestCase {
     public void testStripLastOidPart() {
