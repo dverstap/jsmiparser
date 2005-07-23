1b2db4d787c17663d7ab80d364e9c350bbea7e54
Davy Verstappen <davy@davyverstappen.me>
2005-07-23 11:20:53 +0000

    git-svn-id: http://jsmiparser.svn.sf.net/svnroot/jsmiparser/trunk@24 5cfb4343-f3fb-0310-ad28-926a00f43e72

--- jsmiparser-api/src/main/java/org/jsmiparser/phase/file/ParserPhaseOptions.java
+++ jsmiparser-api/src/main/java/org/jsmiparser/phase/file/ParserPhaseOptions.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jsmiparser.parser;
+package org.jsmiparser.phase.file;
 
 import org.apache.log4j.Logger;
 
