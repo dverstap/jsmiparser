c1816343c07135665c69310c4eaf6ccb13ebdd9b
Davy Verstappen <davy@davyverstappen.me>
2006-08-04 18:07:20 +0000

    cleanup maven files
    git-svn-id: http://jsmiparser.svn.sf.net/svnroot/jsmiparser/trunk@105 5cfb4343-f3fb-0310-ad28-926a00f43e72

--- jsmiparser-api/src/main/java/org/jsmiparser/phase/xref/XRefPhase.java
+++ jsmiparser-api/src/main/java/org/jsmiparser/phase/xref/XRefPhase.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2006 Davy Verstappen.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jsmiparser.phase.xref;
 
 import org.jsmiparser.phase.Phase;
@@ -7,13 +22,6 @@ import org.jsmiparser.smi.SmiMib;
 import org.jsmiparser.smi.SmiTable;
 import org.jsmiparser.smi.SmiModule;
 
-/**
- * Created by IntelliJ IDEA.
- * User: stappend
- * Date: Aug 3, 2006
- * Time: 12:23:22 PM
- * To change this template use File | Settings | File Templates.
- */
 public class XRefPhase implements Phase {
 
     private ProblemReporterFactory m_problemReporterFactory;
