7a1cdaddc8ee8c42882553709f2c3a86b3cf4198
Jeff Gehlbach <jeffg@opennms.org>
2012-10-04 18:04:17 -0400

    Correct copyright holder on new classes

--- jsmiparser-api/src/main/java/org/jsmiparser/smi/SmiTrapType.java
+++ jsmiparser-api/src/main/java/org/jsmiparser/smi/SmiTrapType.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005 Davy Verstappen.
+ * Copyright 2012 The OpenNMS Group, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
