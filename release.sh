bbe10d8909e1c132412bff2f0714e9fd17325cc9
Davy Verstappen <davy@wintersleep.org>
2017-07-14 09:33:57 +0200

    Use ./gradelw in release.sh

--- release.sh
+++ release.sh
@@ -17,11 +17,9 @@ git tag $VERSION
 git archive --format=tar --prefix=$SRC_DIST/ $VERSION | tar xf -
 
 cd $SRC_DIST
-gradle -Pversion=$VERSION clean install stest uploadArchives
+./gradlew -Pversion=$VERSION clean install stest uploadArchives
 
-git push --tags origin $VERSION
-
-echo "Successfully released $VERSION to origin and the staging repo."
+echo "Successfully released $VERSION to the staging repo."
 echo "After verification you still need to:"
-echo "1. git push --tags github $VERSION"
-echo "2. Release the staging repo at http://oss.sonatype.org"
+echo "1. Release the staging repo at http://oss.sonatype.org"
+echo "2. git push --tags origin $VERSION"
